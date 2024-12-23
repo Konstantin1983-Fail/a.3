8906a2d94763c69e5515a15f588942579507a0b6
Konstantin <kostiieo830@gmail.com>
2024-12-23 22:38:17 +0400

    Цель: научится писать необходимую валидацию для вводимых данных при помощи классов Path и Annotated.

---
+++ module_16_2.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get('/user/{user_id}')
+async def get_user(
+    user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)]
+):
+    return {"user_id": user_id}
+
+@app.get('/user/{username}/{age}')
+async def get_user_details(
+    username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+    age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]
+):
+    return {"username": username, "age": age}
+
